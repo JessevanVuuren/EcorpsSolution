ddf388dd69dccf2c169517ae5fb3fae84007e479
Jesse van Vuuren <32089540+JessevanVuuren@users.noreply.github.com>
2023-01-07 20:46:14 +0100

    fix: spelling mistake

--- src/main/java/com/Ecorp/solution/security/SecurityConfig.java
+++ src/main/java/com/Ecorp/solution/security/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import javax.servlet.http.HttpServletResponse;
 
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
